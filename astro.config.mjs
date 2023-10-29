53714c6594c5eb362dd2978f21b5c1352ea3de82
humo-tech <yano.wni+git@gmail.com>
2023-10-29 01:34:31 +0000

    github pages 対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://humo-tech.github.io/GLSLSchool/',
+  outDir: './docs',
   integrations: [vue(), sitemap()],
 })
