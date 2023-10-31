9de1fa226b1fe7d1874f53dd31654467073e434d
humo-tech <yano.wni+git@gmail.com>
2023-10-31 16:16:39 +0000

    ハロウィン！

---
+++ src/components/tasks/01_3/shaders/vert.glsl
@@ -0,0 +1,17 @@
+attribute vec2 position;
+attribute vec4 color;
+uniform vec2 mousePos;
+varying vec4 vColor;
+
+void main (void) {
+    vColor = color;
+
+    vec2 toMouse = mousePos - position;
+    float distanceToMouse = length(toMouse);
+    vec2 normalizedToMouse = normalize(toMouse);
+    vec2 offset = normalizedToMouse * 0.1 * (1.0 - distanceToMouse);
+    vec2 p = position - offset;
+
+    gl_Position = vec4(p, 0.0, 1.0);
+    gl_PointSize = 5.0;
+}
